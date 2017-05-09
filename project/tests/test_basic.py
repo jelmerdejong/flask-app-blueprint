b87b342cad9ef8042b305675ed16e88291162e86
Jelmer de Jong <jelmerjtdejong@gmail.com>
2017-05-09 15:28:16 -0400

    Testing infra added, plus basic coverage, fixes #5

--- project/tests/test_basic.py
+++ project/tests/test_basic.py
@@ -4,7 +4,7 @@
 import os
 import unittest
 
-from project import app, db, mail
+from project import app, db
 
 class BasicTests(unittest.TestCase):
 
@@ -17,13 +17,10 @@ class BasicTests(unittest.TestCase):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-            os.path.join(app.config['BASEDIR'], TEST_DB)
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/test'
         self.app = app.test_client()
         db.drop_all()
         db.create_all()
-
-        mail.init_app(app)
         self.assertEqual(app.debug, False)
 
     # executed after each test
@@ -67,7 +64,7 @@ class BasicTests(unittest.TestCase):
     def test_valid_user_registration(self):
         response = self.register('test11@test11.com', 'PasswIsGood13#$', 'PasswIsGood13#$')
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'Thanks for registering!', response.data)
+        self.assertIn(b'<strong>Success!</strong> Thanks for registering.', response.data)
 
     def test_invalid_user_registration_different_passwords(self):
         response = self.register('test22@test22.com', 'Pass129', 'Different')
@@ -76,8 +73,8 @@ class BasicTests(unittest.TestCase):
     def test_invalid_user_registration_duplicate_email(self):
         response = self.register('test33@test33.com', '%^#@12sa', '%^#@12sa')
         self.assertEqual(response.status_code, 200)
-        response = self.register('test33@test33.com', '%^#@12sa', '%^#@12sa')
-        self.assertIn(b'ERROR! Email (test33@test33.com) already exists.', response.data)
+        response = self.register('test33@test33.com', '89298dka', '89298dka')
+        self.assertIn(b'<strong>Error!</strong> Unable to process registration.', response.data)
 
 
 if __name__ == "__main__":
