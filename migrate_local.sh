0f4c1b1f3636218d933bbefe8aa51a3285548ca5
kimJenny <qsa@kakao.com>
2021-05-17 03:25:05 +0000

    migrate path error fix

--- migrate_local.sh
+++ migrate_local.sh
@@ -1,2 +1,3 @@
-APP_SETTINGS=config.DevelopmentConfig  SECRET_KEY=supersecret  SQLALCHEMY_DATABASE_URI=sqlite:///testcode.db python3 manage.py db init
-APP_SETTINGS=config.DevelopmentConfig  SECRET_KEY=supersecret  SQLALCHEMY_DATABASE_URI=sqlite:///testcode.db python3 manage.py db migrate
+cd project
+APP_SETTINGS=config.DevelopmentConfig  SECRET_KEY=supersecret  SQLALCHEMY_DATABASE_URI=sqlite:///testcode.db python3 ../manage.py db init
+APP_SETTINGS=config.DevelopmentConfig  SECRET_KEY=supersecret  SQLALCHEMY_DATABASE_URI=sqlite:///testcode.db python3 ../manage.py db migrate
