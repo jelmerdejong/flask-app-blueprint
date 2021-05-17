eaf0169f63b1cd7d2ef7055e63170b0025cb3e93
kimJenny <qsa@kakao.com>
2021-05-17 02:58:07 +0000

    add migrate command

---
+++ migrate_local.sh
@@ -0,0 +1,2 @@
+APP_SETTINGS=config.DevelopmentConfig  SECRET_KEY=supersecret  SQLALCHEMY_DATABASE_URI=sqlite:///testcode.db python3 manage.py db init
+APP_SETTINGS=config.DevelopmentConfig  SECRET_KEY=supersecret  SQLALCHEMY_DATABASE_URI=sqlite:///testcode.db python3 manage.py db migrate
